b6712afd5f690b0938dbc542ee97a092738fbdf1
Dev Goldy <jassim7256@gmail.com>
2024-02-24 22:25:45 +0000

    feat: added range to ``random_character_count``

--- src/events/keyboard.rs
+++ src/events/keyboard.rs
@@ -5,12 +5,12 @@ pub fn keyboard_type(mut windows: Query<&mut Window>) {
     let keyboard_enigo = enigo::Enigo::new();
 
     let mut rng = rand::thread_rng();
-    let random_character_count: u8 = rng.gen();
+    let random_character_count: u8 = rng.gen_range(16..56);
 
     let random_characters: String = rng.sample_iter(&Alphanumeric)
         .take(random_character_count.into())
         .map(char::from)
         .collect();
 
-    println!("{}", random_characters);
+    println!(">> {}", random_characters);
 }
